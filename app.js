c247fb234dceee2896b7f4a13d93ff99c91030d1
joy2be <joy2behere@gmail.com>
2024-09-03 17:44:37 -0400

    Update app.js

--- app.js
+++ app.js
@@ -13,25 +13,3 @@ switcher.addEventListener('click', function () {
     }
     console.log('current class name: ' + className);
 });
-
-    $(document).ready(function() {
-      // Retrieve JSON data from "ScanTestData.json" file
-      $.getJSON("ScanTestData.json", function(data) {
-        var tableBody = $("#myTable tbody");
-
-        // Iterate over each person object in the JSON data
-        $.each(data, function(index, items) {
-          var row = $("<tr></tr>"); // Create a new table row
-
-          // Create table cells and fill them with the person's data
-          row.append($("<td></td>").text(items.scanid));
-          row.append($("<td></td>").text(items.current_loc));
-          row.append($("<td></td>").text(items.scanned_itemid));
-          row.append($("<td></td>").text(items.scantimestamp));
-          row.append($("<td></td>").text(items.invtype));
-          row.append($("<td></td>").text(items.machname));
-
-          tableBody.append(row); // Add the row to the table body
-        });
-      });
-    });
