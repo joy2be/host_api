378149e6eeec710682f4b01f1e501105e7a0fa4f
joy2be <joy2behere@gmail.com>
2024-09-03 16:30:41 -0400

    Update app.js

--- app.js
+++ app.js
@@ -5,14 +5,37 @@ const switcher = document.querySelector('.btnTheme');
 switcher.addEventListener('click', function () {
     document.body.classList.toggle('light-theme');
     document.body.classList.toggle('dark-theme');
-
     const className = document.body.className;
     if (className == "light-theme") {
         this.textContent = "Dark";
     } else {
         this.textContent = "Light";
     }
-
     console.log('current class name: ' + className);
-
 });
+
+var myArray = []
+$.ajax({
+    method: 'GET',
+    url: 'https://joy2be.github.io/host_api/ScanTestData.json',
+    success: function(response){
+        myArray = response.items
+        buildTable(myArray)
+        console.log(MyArray)
+    }
+})
+      
+function buildTable(jsonData){
+    var table = document.getElementById('myTable')
+    for (var i = 0; i < jsonData.length; i++) {
+        var row = '<tr>
+                    <td>${jsonData[i].scanid}</td>
+                    <td>${jsonData[i].current_loc}</td>
+                    <td>${jsonData[i].scanned_itemid}</td>
+                    <td>${jsonData[i].scantimestamp}</td>
+                    <td>${jsonData[i].invtype}</td>
+                    <td>${jsonData[i].machname}</td>
+                </tr>'
+        table.innerHTML += row;
+    }
+}
