ccff895f22a8af6aa4f3e445c22761b5474868e0
joy2be <joy2behere@gmail.com>
2024-09-25 17:35:15 -0400

    Added scripts

---
+++ dataFuncs.js
@@ -0,0 +1,10 @@
+﻿import BDXM_API_KEY from "./apikey.js";
+
+export const getWeatherFromCoords = async (locationObj) => {
+  try {
+	  const url = buildApiCoordsUrl(locationObj);
+	  return await getWeatherConditions(url);
+  } catch (err) {
+	  console.error(err.stack);
+  }
+};
