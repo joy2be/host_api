bd06f69f42d12bc2525871ad9bccbb5f399b47a5
joy2be <joy2behere@gmail.com>
2024-10-31 11:25:16 -0400

    Test Adding Hidden js Files

--- dataFuncs.js
+++ dataFuncs.js
@@ -1,10 +1,1 @@
 ﻿import BDXM_API_KEY from "./apikey.js";
-
-export const getWeatherFromCoords = async (locationObj) => {
-  try {
-	  const url = buildApiCoordsUrl(locationObj);
-	  return await getWeatherConditions(url);
-  } catch (err) {
-	  console.error(err.stack);
-  }
-};
